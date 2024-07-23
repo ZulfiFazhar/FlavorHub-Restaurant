9703f014f17e912a709b11ef27b9f4abf59e0f9e
Zulfi Fadilah Azhar <zulfi.fazhar12@gmail.com>
2024-07-23 15:02:39 +0700

    feat: adding list Menu

--- src/app/menu/page.jsx
+++ src/app/menu/page.jsx
@@ -2,6 +2,8 @@
 
 import React, { useState, useEffect } from "react";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import Navbar from "@/components/navbar";
+import MenuCard from "@/components/menu/MenuCard";
 
 function Page() {
   const [menu, setMenu] = useState([]);
@@ -22,11 +24,20 @@ function Page() {
   });
 
   return (
-    <div>
-      {menu.map((mn) => {
+    <div className="h-min-full bg-white text-black">
+      <Navbar />
+      <div className="w-full flex justify-center">
+        <div className="w-4/5 p-6 md:p-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {menu.map((item) => (
+            <MenuCard item={item} key={item.id} />
+          ))}
+        </div>
+      </div>
+
+      {menu.map((menu) => {
         return (
-          <div key={mn.id} className="mb-10">
-            {JSON.stringify(mn)}
+          <div key={menu.id} className="mb-10">
+            {JSON.stringify(menu)}
           </div>
         );
       })}
--- src/app/resto/page.jsx
+++ src/app/resto/page.jsx
@@ -1,34 +1,30 @@
-"use client"
+"use client";
 
 import React from "react";
 import { usePekerjaanContext } from "./providers";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const pekerjaan = usePekerjaanContext()
-  const router = useRouter()
+  const pekerjaan = usePekerjaanContext();
+  const router = useRouter();
   let content;
   switch (pekerjaan) {
     case "pelayan":
-      router.push('/resto/pelayan')
+      router.push("/resto/pelayan");
       break;
     case "koki":
-      router.push('/resto/koki')
+      router.push("/resto/koki");
       break;
     case "kasir":
-      router.push('/resto/kasir')
+      router.push("/resto/kasir");
       break;
     case "manajer":
-      router.push('/resto/manajer')
+      router.push("/resto/manajer");
       break;
     default:
-      content = <div className="h-screen text-3xl">Selamat datang</div>
+      content = <div className="h-screen text-3xl">Selamat datang</div>;
       break;
   }
 
-  return (
-    <>
-        {content}
-    </>
-  );
+  return <>{content}</>;
 }
